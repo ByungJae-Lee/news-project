d647244f6d0c3a53ba6750a404487c462a1900a4
banya <bjgeumgang@gmail.com>
2024-02-19 21:35:01 +0900

    news-project_addfile

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let news = [];
 const getLatestNews = async () => {
   const url =
     // URL인스턴스는 url에 필요한 함수와 변수들을 제공함
-    new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}
+    new URL(`https://newstimesbbc.netlify.app//top-headlines
   `);
   console.log(url);
   const response = await fetch(url);
