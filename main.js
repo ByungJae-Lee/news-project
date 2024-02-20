e267403e861a70c6c62e445fca194925c1dd0713
banya <bjgeumgang@gmail.com>
2024-02-21 00:40:58 +0900

    news-project_add-search bar

--- main.js
+++ main.js
@@ -17,6 +17,15 @@ const getLatestNews = async () => {
 
 getLatestNews();
 
+// 햄버거메뉴 V
+// 누나가 사용한 것은 Sidenav overlay V
+// 데스크탑 모드일 때 햄버거메뉴 안보임 V
+// 모바일 모드일 때 메뉴들이 사라지고 햄버거가 나타남 V
+// 햄버거를 클릭하면 메뉴들이 사이드바로 나타남 메뉴들은 세로정렬 V
+
+// 인풋, Go버튼은 평소에는 숨겨져있다 V
+// 검색아이콘을 클릭하면 인풋, Go버튼이 나타난다 V
+
 // 햄버거 클릭시 사이드바 open
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -25,11 +34,21 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
-// 햄버거메뉴 V
-// 누나가 사용한 것은 Sidenav overlay V
-// 데스크탑 모드일 때 햄버거메뉴 안보임 V
-// 모바일 모드일 때 메뉴들이 사라지고 햄버거가 나타남 V
-// 햄버거를 클릭하면 메뉴들이 사이드바로 나타남 메뉴들은 세로정렬 V
+// 검색창 껏다키는 toggle기능 구현
+let isHidden = false;
+let searchInput = document.getElementById("serch-input");
+let goBtn = document.getElementById("go-btn");
 
-// 인풋, Go버튼은 평소에는 숨겨져있다
-// 검색아이콘을 클릭하면 인풋, Go버튼이 나타난다
+function openSch() {
+  if (isHidden) {
+    searchInput.style.visibility = "visible";
+    goBtn.style.visibility = "visible";
+    isHidden = false;
+  } else {
+    searchInput.style.visibility = "hidden";
+    goBtn.style.visibility = "hidden";
+    isHidden = true;
+  }
+}
+
+function render() {}
